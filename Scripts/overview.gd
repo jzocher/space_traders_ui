21189ba2a131833036f3c4148e094e35bf8627e2
Jeffrey Zocher <jeff.zocher@gmail.com>
2023-05-13 16:38:42 -0500

    More overview work

--- Scripts/overview.gd
+++ Scripts/overview.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var anim_player = $AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MarginContainer/VBoxContainer/HBoxContainer/GreetingLabel.text += agent_info.agent_name
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_menu_toggle_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if $HBoxContainer/Sidebar.custom_minimum_size[0] == 70:
+				anim_player.play("SidebarAnimation")
+			else:
+				anim_player.play_backwards("SidebarAnimation")
