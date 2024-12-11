eda4b1467f41a386cede7bb4d9d98a975488b500
Jeffrey Zocher <jeff.zocher@gmail.com>
2024-12-11 00:11:43 -0600

    Fixed registartion, added faction information to registration page, tweaked some text.

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,13 +1,29 @@
 extends Control
 
+@onready var req = $HTTPRequest  
 # @onready var agent_info = get_node("/root/agent_info")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$ControlPanel/ChoiceMenu.visible = true
 	$ControlPanel/RegisterMenu.visible = false
 	$ControlPanel/LoginMenu.visible = false
+	
+	req.request_completed.connect(_on_version_request_completed)
+	var error = req.request("https://api.spacetraders.io/v2")
+	if error != OK:
+		print("Failed to send request.")
+	else:
+		print("Request sent successfully.")
+	
 
-
+func _on_version_request_completed(result, response_code, _headers, body):
+	if response_code == 200:
+		var json = JSON.parse_string(body.get_string_from_utf8())
+		$Version.text = ("Version: " + json["version"])
+		$LastReset.text = ("Last Reset: " + json["resetDate"])
+	else:
+		print("Failed to fetch game data")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
@@ -17,11 +33,12 @@ func _on_login_btn_pressed():
 	if agent_token != "":
 		print(agent_token)
 		var options = ["Content-Type: application/json","Authorization: Bearer "+agent_token]
-		$HTTPRequest.request_completed.connect(_on_login_request_completed)
-		$HTTPRequest.request("https://api.spacetraders.io/v2/my/agent", options)
+		req.request_completed.connect(_on_login_request_completed)
+		req.request("https://api.spacetraders.io/v2/my/agent", options)
 
 func _on_login_request_completed(result, response_code, _headers, body):
-	if result == $HTTPRequest.RESULT_SUCCESS:
+	print("result: ",result, "response_code: ", response_code)
+	if result == req.RESULT_SUCCESS and response_code == 200:
 		var json = JSON.parse_string(body.get_string_from_utf8())
 		print("Success:\n",json,"\n",result,"\n", response_code)
 		agent_info.account_id = json["data"]["accountId"]
@@ -29,8 +46,10 @@ func _on_login_request_completed(result, response_code, _headers, body):
 		agent_info.headquarters = json["data"]["headquarters"]
 		agent_info.credits = json["data"]["credits"]
 		get_tree().change_scene_to_file("res://Scenes/overview.tscn")
+	elif response_code == 401:
+		print("No trader found")
 	else:
-		print("Error:\n",result, response_code)
+		print("Unknown: ", result, " ", response_code)
 
 
 func _on_register_btn_pressed():
@@ -40,12 +59,12 @@ func _on_register_btn_pressed():
 		var url = "https://api.spacetraders.io/v2/register"
 		var headers = ["Content-Type: application/json"]
 		var body = JSON.stringify({"symbol":reg_username,"faction":reg_faction})
-		$HTTPRequest.request_completed.connect(_on_register_request_completed)
-		$HTTPRequest.request(url, headers, true, HTTPClient.METHOD_POST, body)
+		req.request_completed.connect(_on_register_request_completed)
+		req.request(url, headers, HTTPClient.METHOD_POST, body)
 		
 		
 func _on_register_request_completed(result, response_code, _headers, body):
-	if result == $HTTPRequest.RESULT_SUCCESS:
+	if result == req.RESULT_SUCCESS:
 		var json = JSON.parse_string(body.get_string_from_utf8())
 		print(json)
 	else:
@@ -68,3 +87,10 @@ func _on_back_btn_pressed():
 	$ControlPanel/RegisterMenu.visible = false
 	$ControlPanel/LoginMenu.visible = false
 
+
+func _on_factions_info_toggled(toggled_on):
+	$ControlPanel/RegisterMenu/Factions.visible = true
+
+
+func _on_factions_info_pressed():
+	$ControlPanel/RegisterMenu/Factions.visible = !$ControlPanel/RegisterMenu/Factions.visible
--- agent_info.gd
+++ agent_info.gd
@@ -5,3 +5,281 @@ var account_id = ""
 var agent_name = ""
 var headquarters = ""
 var credits = 0
+var starting_faction = ""
+var ship_count = 0
+var contracts = {}
+var current_faction = {}
+var current_ship = {}
+
+
+#var "token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiUkVIQ09aIiwidmVyc2lvbiI6InYyLjIuMCIsInJlc2V0X2RhdGUiOiIyMDI0LTEwLTI3IiwiaWF0IjoxNzMzODc5MTY5LCJzdWIiOiJhZ2VudC10b2tlbiJ9.a2OV9pLqNO_3dvh7wQk2TtGNQhkvlwOEtMs5rqX3P5lSpbwRSCHy24uKHVhgf2E2LSK08GbTfdh9a6VUL-Vq-TANSoPo5TU8a10CUc0lkwtzNS_zFJKBOZ3E63txd8D-ZQhzdodQN7b_qoypQFDm5KVzOOZCw71N-IynKeqeiUp3TxJGS3sfUDGotPBfFH95vgQvFhDzfry6BVqvXIDyfoZwr3Cm7biUL8d3GZLvosabrfEFh6hsXk2MWQXbFWIbrqeXaVk-9p_SsaKWjPs9BJGiM2ynN1ppTNsuXEW7rytr3Ad2D85yT0H2rw_EHurp8Y6pD-9TzPyCUG4GzIfWaw",
+#var "agent": {
+	  #"accountId": "cm4j6w1jq011cs60csaj2uiq9",
+	  #"symbol": "REHCOZ",
+	  #"headquarters": "X1-J69-A1",
+	  #"credits": 175000,
+	  #"startingFaction": "COSMIC",
+	  #"shipCount": 0
+	#},
+	#"contract": {
+	  #"id": "cm4j6w1lp011es60ctzkfrmt5",
+	  #"factionSymbol": "COSMIC",
+	  #"type": "PROCUREMENT",
+	  #"terms": {
+		#"deadline": "2024-12-18T01:06:09.542Z",
+		#"payment": {
+		  #"onAccepted": 2109,
+		  #"onFulfilled": 12656
+		#},
+		#"deliver": [
+		  #{
+			#"tradeSymbol": "ALUMINUM_ORE",
+			#"destinationSymbol": "X1-J69-H55",
+			#"unitsRequired": 59,
+			#"unitsFulfilled": 0
+		  #}
+		#]
+	  #},
+	  #"accepted": false,
+	  #"fulfilled": false,
+	  #"expiration": "2024-12-12T01:06:09.542Z",
+	  #"deadlineToAccept": "2024-12-12T01:06:09.542Z"
+	#},
+	#"faction": {
+	  #"symbol": "COSMIC",
+	  #"name": "Cosmic Engineers",
+	  #"description": "The Cosmic Engineers are a group of highly advanced scientists and engineers who seek to terraform and colonize new worlds, pushing the boundaries of technology and exploration.",
+	  #"headquarters": "X1-FZ42",
+	  #"traits": [
+		#{
+		  #"symbol": "INNOVATIVE",
+		  #"name": "Innovative",
+		  #"description": "Willing to try new and untested ideas. Sometimes able to come up with creative and original solutions to problems, and may be able to think outside the box. Sometimes at the forefront of technological or social change, and may be willing to take risks in order to advance the boundaries of human knowledge and understanding."
+		#},
+		#{
+		  #"symbol": "BOLD",
+		  #"name": "Bold",
+		  #"description": "Unafraid to take risks and challenge the status quo. Sometimes willing to do things that others would not dare, and may be able to overcome obstacles and challenges that would be insurmountable for others. Sometimes able to inspire and motivate others to take bold action as well."
+		#},
+		#{
+		  #"symbol": "VISIONARY",
+		  #"name": "Visionary",
+		  #"description": "Possessing a clear and compelling vision for the future. Sometimes able to see beyond the present and anticipate the needs and challenges of tomorrow. Sometimes able to inspire and guide others towards a better and brighter future, and may be willing to take bold and decisive action to make their vision a reality."
+		#},
+		#{
+		  #"symbol": "CURIOUS",
+		  #"name": "Curious",
+		  #"description": "Possessing a strong desire to learn and explore. Sometimes interested in a wide range of topics and may be willing to take risks in order to satisfy their curiosity. Sometimes able to think outside the box and come up with creative solutions to challenges."
+		#}
+	  #],
+	  #"isRecruiting": true
+	#},
+	#"ship": {
+	  #"symbol": "REHCOZ-1",
+	  #"nav": {
+		#"systemSymbol": "X1-J69",
+		#"waypointSymbol": "X1-J69-A1",
+		#"route": {
+		  #"origin": {
+			#"symbol": "X1-J69-A1",
+			#"type": "PLANET",
+			#"systemSymbol": "X1-J69",
+			#"x": -15,
+			#"y": -19
+		  #},
+		  #"destination": {
+			#"symbol": "X1-J69-A1",
+			#"type": "PLANET",
+			#"systemSymbol": "X1-J69",
+			#"x": -15,
+			#"y": -19
+		  #},
+		  #"arrival": "2024-12-11T01:06:09.594Z",
+		  #"departureTime": "2024-12-11T01:06:09.594Z"
+		#},
+		#"status": "DOCKED",
+		#"flightMode": "CRUISE"
+	  #},
+	  #"crew": {
+		#"current": 57,
+		#"capacity": 80,
+		#"required": 57,
+		#"rotation": "STRICT",
+		#"morale": 100,
+		#"wages": 0
+	  #},
+	  #"fuel": {
+		#"current": 400,
+		#"capacity": 400,
+		#"consumed": {
+		  #"amount": 0,
+		  #"timestamp": "2024-12-11T01:06:09.594Z"
+		#}
+	  #},
+	  #"cooldown": {
+		#"shipSymbol": "REHCOZ-1",
+		#"totalSeconds": 0,
+		#"remainingSeconds": 0
+	  #},
+	  #"frame": {
+		#"symbol": "FRAME_FRIGATE",
+		#"name": "Frigate",
+		#"description": "A medium-sized, multi-purpose spacecraft, often used for combat, transport, or support operations.",
+		#"moduleSlots": 8,
+		#"mountingPoints": 5,
+		#"fuelCapacity": 400,
+		#"condition": 1,
+		#"integrity": 1,
+		#"requirements": {
+		  #"power": 8,
+		  #"crew": 25
+		#}
+	  #},
+	  #"reactor": {
+		#"symbol": "REACTOR_FISSION_I",
+		#"name": "Fission Reactor I",
+		#"description": "A basic fission power reactor, used to generate electricity from nuclear fission reactions.",
+		#"condition": 1,
+		#"integrity": 1,
+		#"powerOutput": 31,
+		#"requirements": {
+		  #"crew": 8
+		#}
+	  #},
+	  #"engine": {
+		#"symbol": "ENGINE_ION_DRIVE_II",
+		#"name": "Ion Drive II",
+		#"description": "An advanced propulsion system that uses ionized particles to generate high-speed, low-thrust acceleration, with improved efficiency and performance.",
+		#"condition": 1,
+		#"integrity": 1,
+		#"speed": 30,
+		#"requirements": {
+		  #"power": 6,
+		  #"crew": 8
+		#}
+	  #},
+	  #"modules": [
+		#{
+		  #"symbol": "MODULE_CARGO_HOLD_II",
+		  #"name": "Expanded Cargo Hold",
+		  #"description": "An expanded cargo hold module that provides more efficient storage space for a ship's cargo.",
+		  #"capacity": 40,
+		  #"requirements": {
+			#"crew": 2,
+			#"power": 2,
+			#"slots": 2
+		  #}
+		#},
+		#{
+		  #"symbol": "MODULE_CREW_QUARTERS_I",
+		  #"name": "Crew Quarters",
+		  #"description": "A module that provides living space and amenities for the crew.",
+		  #"capacity": 40,
+		  #"requirements": {
+			#"crew": 2,
+			#"power": 1,
+			#"slots": 1
+		  #}
+		#},
+		#{
+		  #"symbol": "MODULE_CREW_QUARTERS_I",
+		  #"name": "Crew Quarters",
+		  #"description": "A module that provides living space and amenities for the crew.",
+		  #"capacity": 40,
+		  #"requirements": {
+			#"crew": 2,
+			#"power": 1,
+			#"slots": 1
+		  #}
+		#},
+		#{
+		  #"symbol": "MODULE_MINERAL_PROCESSOR_I",
+		  #"name": "Mineral Processor",
+		  #"description": "Crushes and processes extracted minerals and ores into their component parts, filters out impurities, and containerizes them into raw storage units.",
+		  #"requirements": {
+			#"crew": 0,
+			#"power": 1,
+			#"slots": 2
+		  #}
+		#},
+		#{
+		  #"symbol": "MODULE_GAS_PROCESSOR_I",
+		  #"name": "Gas Processor",
+		  #"description": "Filters and processes extracted gases into their component parts, filters out impurities, and containerizes them into raw storage units.",
+		  #"requirements": {
+			#"crew": 0,
+			#"power": 1,
+			#"slots": 2
+		  #}
+		#}
+	  #],
+	  #"mounts": [
+		#{
+		  #"symbol": "MOUNT_SENSOR_ARRAY_II",
+		  #"name": "Sensor Array II",
+		  #"description": "An advanced sensor array that improves a ship's ability to detect and track other objects in space with greater accuracy and range.",
+		  #"strength": 4,
+		  #"requirements": {
+			#"crew": 2,
+			#"power": 2
+		  #}
+		#},
+		#{
+		  #"symbol": "MOUNT_GAS_SIPHON_II",
+		  #"name": "Gas Siphon II",
+		  #"description": "An advanced gas siphon that can extract gas from gas giants and other gas-rich bodies more efficiently and at a higher rate.",
+		  #"strength": 20,
+		  #"requirements": {
+			#"crew": 2,
+			#"power": 2
+		  #}
+		#},
+		#{
+		  #"symbol": "MOUNT_MINING_LASER_II",
+		  #"name": "Mining Laser II",
+		  #"description": "An advanced mining laser that is more efficient and effective at extracting valuable minerals from asteroids and other space objects.",
+		  #"strength": 5,
+		  #"requirements": {
+			#"crew": 2,
+			#"power": 2
+		  #}
+		#},
+		#{
+		  #"symbol": "MOUNT_SURVEYOR_II",
+		  #"name": "Surveyor II",
+		  #"description": "An advanced survey probe that can be used to gather information about a mineral deposit with greater accuracy.",
+		  #"strength": 2,
+		  #"deposits": [
+			#"QUARTZ_SAND",
+			#"SILICON_CRYSTALS",
+			#"PRECIOUS_STONES",
+			#"ICE_WATER",
+			#"AMMONIA_ICE",
+			#"IRON_ORE",
+			#"COPPER_ORE",
+			#"SILVER_ORE",
+			#"ALUMINUM_ORE",
+			#"GOLD_ORE",
+			#"PLATINUM_ORE",
+			#"DIAMONDS",
+			#"URANITE_ORE"
+		  #],
+		  #"requirements": {
+			#"crew": 4,
+			#"power": 3
+		  #}
+		#}
+	  #],
+	  #"registration": {
+		#"name": "REHCOZ-1",
+		#"factionSymbol": "COSMIC",
+		#"role": "COMMAND"
+	  #},
+	  #"cargo": {
+		#"capacity": 40,
+		#"units": 0,
+		#"inventory": []
+	  #}
+	#}
+  #}
+#}
